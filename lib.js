4c339a3fa9a819a8e3ffb7a7b13fe7c73300e76f
lima02 <mig.c.lima@gmail.com>
2018-06-28 10:39:13 +1000

    initial commit

--- lib.js
+++ lib.js
@@ -5,14 +5,32 @@ const jwt = require('jsonwebtoken');
 const util = require('util');
 
 const getPolicyDocument = (effect, resource) => {
+    const api_arn = resource.split('/')[0];
+    const stage = resource.split('/')[1];
+
     const policyDocument = {
         Version: '2012-10-17', // default version
         Statement: [{
             Action: 'execute-api:Invoke', // default action
             Effect: effect,
-            Resource: resource,
+            Resource: `${api_arn}/${stage}/*`, // granting access to all methods in the specified stage
+            /* authoriser caching issue
+            https://forums.aws.amazon.com/thread.jspa?threadID=225934&tstart=0
+            This error happens if you specify event.methodArn as resource in the generated policy,
+            but want to use the same Lambda function for multiple methods in the API. The first valid
+            call to method 'A' returns a policy that is cached. The same policy is returned even when
+            the call is for method 'B' because the same token is used. But since the event.methodArn is
+            wrong in the policy, a 403 is returned. 
+
+            The solution is to then to not reference the event.methodArn in the policy. Instead, use a 
+            wildcard to cover all the methods that the token will be used for like below:
+            arn:aws:execute-api:eu-west-1:1234567890:abcdefghi/prod/*
+
+            Don't disable cacheing. Unless you have money to waste.
+            */
         }]
     };
+    
     return policyDocument;
 }
 
@@ -41,7 +59,6 @@ const jwtOptions = {
 };
 
 module.exports.authenticate = (params) => {
-    console.log(params);
     const token = getToken(params);
 
     const decoded = jwt.decode(token, { complete: true });
@@ -53,7 +70,7 @@ module.exports.authenticate = (params) => {
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 10, // Default value
-        jwksUri: process.env.JWKS_URI
+        jwksUri: `${process.env.TOKEN_ISSUER}.well-known/jwks.json`
     });
 
     const getSigningKey = util.promisify(client.getSigningKey);
@@ -62,9 +79,9 @@ module.exports.authenticate = (params) => {
             const signingKey = key.publicKey || key.rsaPublicKey;
             return jwt.verify(token, signingKey, jwtOptions);
         })
-        .then((decoded)=> ({
-            principalId: decoded.sub,
-            policyDocument: getPolicyDocument('Allow', params.methodArn),
-            context: { scope: decoded.scope }
+        .then((decoded)=> Object.assign({}, {
+            'principalId': decoded.sub,
+            'policyDocument': getPolicyDocument('Allow', params.methodArn),
+            'context': { 'scope': decoded.scope }
         }));
 }
